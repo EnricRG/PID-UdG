be272d2fc38c660b9404f7bbd62c63240ad3f944
EnricRG <u1939673@campus.udg.edu>
2019-04-19 11:57:12 +0200

    Added missing headers to include folders

---
+++ installation/Linux/include/windowAddon/TurtleMotion.h
@@ -0,0 +1,20 @@
+#ifndef TURTLEMOTION_H
+#define TURTLEMOTION_H
+
+#include "WindowSpec.h"
+#include "TurtleMove.h"
+#include "Point2D.h"
+
+static const int WRAP_MODE = 1;		//When turtle runs to an edge of the screen, it appears on the opposite edge
+static const int WINDOW_MODE = 2;	//When turtle runs to an edge of the screen, it continues forward outside the screen
+static const int FENCE_MODE = 3;	//When turtle runs to an edge of the screen, it stops
+
+class TurtleMotion 
+{
+public:
+	virtual ~TurtleMotion() {};
+	virtual TurtleMove::const_iterator * computeDestination(Point2D actual_position, double direction, double distance, WindowSpec window) const = 0;
+	virtual int type() const = 0;
+};
+
+#endif
